acced7f90f6a65ee33e1e64ba9cce63e151ef026
GSP-YT <128293212+GSP-YT@users.noreply.github.com>
2023-09-12 20:53:37 +0530

    Add files via upload

--- merg2ArrayShort.java
+++ merg2ArrayShort.java
@@ -5,35 +5,41 @@ class merg2ArrayShort{
 		System.out.println("Enter Size Of Array 1 : ");
 		int n = sc.nextInt();
 		int []array1 = new int[n];
-		System.out.printf("Enter %d Positive Elements Of Array 1 : ",n);
+		System.out.printf("Enter %d Elements Of Array 1 : ",n);
 		for(int i = 0; i < n; i++){
 			array1[i]=sc.nextInt();
 		}
 		System.out.println("Enter Size Of Array 2 : ");
 		int m = sc.nextInt();
 		int []array2 = new int[m];
-		System.out.printf("Enter %d Positive Elements Of Array 2 : ",m);
+		System.out.printf("Enter %d Elements Of Array 2 : ",m);
 		for(int i = 0; i < m; i++){
 			array2[i]=sc.nextInt();
 		}
+		sc.close();
 		int size = n + m;
 		int []array3 = new int[size];
-		if(array1[0] >= array2[0]){
-			array3[0] = array1[0];
-		}
-		else {
-			array3[0] = array2[0];
-		}
-		for(int i = 0; i < size; i++){
-			for(int j = 0; j < size; j++){
-				if(array1[j] >= array2[j]){
-					array3[j] = array1[j];
-				}
-				else {
-					array3[j] = array2[j];
-				}
+		for(int i=0,j=0,k=0; i < size; i++){
+			if(j==n){
+				array3[i] = array2[k];
+				k++;
+				continue;
+			}
+			if(k==m){
+				array3[i] = array1[j];
+				j++;
+				continue;
+			}
+			if(array1[j] < array2[k]){
+				array3[i] = array1[j];
+				j++;
+			}
+			else{
+				array3[i] = array2[k];
+				k++;
 			}
 		}
+		System.out.println("Sorted Merged Array :");
 		for(int j = 0; j < size; j++){
 			System.out.print(array3[j] + "\t");
 		}
