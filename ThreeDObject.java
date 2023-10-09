a7026f09b5a13ac9b840113629ed283554e68ae1
GSP-YT <128293212+GSP-YT@users.noreply.github.com>
2023-10-09 15:49:29 +0530

    Add files via upload

--- ThreeDObject.java
+++ ThreeDObject.java
@@ -1,45 +1,102 @@
 import java.util.Scanner;
-class ThreeDObject{
-	public static void wholeSurface();
-	public static void volume();
+public class W4Q5{
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter Length Breadth Hieght Of Box :");
-		int l = nextInt(), b = nextInt(), h = nextInt();
-		System.out.println("Enter Length Breadth Hieght Of Box :");
-		int l = nextInt(), b = nextInt(), h = nextInt();
+		int l = sc.nextInt();
+		int b = sc.nextInt();
+		int h = sc.nextInt();
+		Box box = new Box(l ,b, h);
 		
+		System.out.println("Enter Size Of Side Of Cube :");
+		int a = sc.nextInt();
+		Cube cube = new Cube(a);
+		
+		System.out.println("Enter Radius Hieght Of Cylinder :");
+		int r = sc.nextInt();
+		l = sc.nextInt();
+		Cylinder cylinder = new Cylinder(r,l);
+		
+		System.out.println("Enter Radius Hieght Of Cone :");
+		r = sc.nextInt();
+		l = sc.nextInt();
+		Cone cone = new Cone(r,l);
+		
+		sc.close();
 	}
 }
-public class Box extends ThreeDObject{
-	public static void wholeSurface(int l,int b,int h){
-		System.out.println("Whole Surface Of Box : "+ 2(l*b + b*h+ l*h) )
+public class ThreeDObject{
+	public void wholeSurface(){}
+	public void volume(){}
+}
+public class Box extends ThreeDObject {
+	int l,b,h;
+	public void wholeSurface(){
+		double x = 2*(l*b + b*h+ l*h);
+		System.out.println("Whole Surface Of Box : "+ x );
 	}
-	public static void volume(int l,int b,int h){
-		System.out.println("Volume Of Box : "+ l*b*h )
+	public void volume(){
+		double x = l*b*h ;
+		System.out.println("Volume Of Box : "+ x );
+	}
+	Box(int a, int d, int c){
+		l = a;
+		b = d;
+		h = c;
+		wholeSurface();
+		volume();
 	}
 }
 public class Cube extends ThreeDObject{
-	public static void wholeSurface(int a){
-		System.out.println("Whole Surface Of Cube : "+ ^6(a*a) )
+	int a;
+	public void wholeSurface(){
+		double x = 6*(a*a);
+		System.out.println("Whole Surface Of Cube : "+ x );
 	}
-	public static void volume(int l,int b,int h){
-		System.out.println("Volume Of Cube : "+ a*a*a )
+	public void volume(){
+		double x = a*a*a;
+		System.out.println("Volume Of Cube : "+ x );
 	}
+	Cube(int x){
+		a = x;
+		wholeSurface();
+		volume();
+	}
+	
 }
 public class Cylinder extends ThreeDObject{
-	public static void wholeSurface(int r,int l){
-		System.out.println("Whole Surface Of Cylinder : "+ 2*3.14*r(r+l) )
+    int r;
+	int l;
+	public void wholeSurface(){
+		double x = 2*3.14*r*(r+l);
+		System.out.println("Whole Surface Of Cylinder : "+ x );
 	}
-	public static void volume(int r,int l){
-		System.out.println("Volume Of Cylinder : "+ 3.14*r*r*l )
+	public void volume(){
+		double x = 3.14*r*r*l ;
+		System.out.println("Volume Of Cylinder : "+ x );
+	}
+	Cylinder(int a, int b){
+		r = a;
+		l = b;
+		wholeSurface();
+		volume();
 	}
 }
 public class Cone extends ThreeDObject{
-	public static void wholeSurface(int r,int l){
-		System.out.println("Whole Surface Of Cone : "+ 3.14*r(r+l) )
+	int r;
+	int l;
+	public void wholeSurface(){
+		double x = 3.14*r*(r+l);
+		System.out.println("Whole Surface Of Cone : "+ x );
 	}
-	public static void volume(int r,int l){
-		System.out.println("Volume Of Cone : "+ 3.14*r*r*l/3 )
+	public void volume(){
+		double x = 3.14*r*r*l/3;
+		System.out.println("Volume Of Cone : "+ x ) ;
+	}
+	Cone(int a, int b){
+		r = a;
+		l = b;
+		wholeSurface();
+		volume();
 	}
 }
